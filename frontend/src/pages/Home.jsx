cbdc2672c749b2aecf0ba4c8af1031eb79f6bd36
Tharusha Rashmika <rashmikatharusha8@gmail.com>
2024-03-21 16:24:43 +0530

    button color change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
                             </button>
                         </Link>
                         <Link to="/MachineHome">
-                            <button className="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
                                 Machine
                             </button>
                         </Link>
@@ -50,7 +50,7 @@ const Home = () => {
                             </button>
                         </Link>
                         <Link to="/OrderHome">
-                            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
                                 Order
                             </button>
                         </Link>
