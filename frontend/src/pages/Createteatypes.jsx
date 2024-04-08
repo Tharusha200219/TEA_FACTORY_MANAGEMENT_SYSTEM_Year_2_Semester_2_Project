b6e1cb2a402e6784107b4955ba2af8c67eb47432
Tharusha Rashmika <rashmikatharusha8@gmail.com>
2024-04-08 16:53:58 +0530

    bug fix
    bug fix in createteatype.jsx

--- frontend/src/pages/Createteatypes.jsx
+++ frontend/src/pages/Createteatypes.jsx
@@ -46,7 +46,7 @@ const Createteatypes = () => {
                         
                         <div className="flex space-x-4">
                             <Link to="/" className="text-black-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link>
-                            <Link to="/Teatypehome" className="">Tea Type</Link>
+                            <Link to="/Teatypehome" className="text-black-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Tea Type</Link>
                             <Link to="/teatypes/creates" className="text-gray-300 bg-black hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">create table</Link>
                             <Link to="/pending-shipments" className="text-black-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">production machine availability</Link>
                             <Link to="/TeaTypeReport" className="text-black-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">tea type report genarate</Link>
