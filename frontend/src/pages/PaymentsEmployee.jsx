6e6553e45852d1fc99ad9cb0522c59448762f2e4
Susiru Sampath Wijesiri <159619704+Susiru24@users.noreply.github.com>
2024-04-17 15:37:29 +0530

    Update PaymentEmployee Table
    Update PaymentEmployee Table

--- frontend/src/pages/PaymentsEmployee.jsx
+++ frontend/src/pages/PaymentsEmployee.jsx
@@ -7,23 +7,35 @@ import { MdOutlineDelete } from 'react-icons/md';
 import Spinner from '../components/Spinner';
 import { useParams } from 'react-router-dom';
 
+
 const PaymentsEmployee = () => {
-    const [suppliers, setSuppliers] = useState([]);
+    const [employees, setEmployee] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredEmployee, setFilteredEmployee] = useState([]);
 
     useEffect(() => {
         setLoading(true);
-        axios.get('http://localhost:5555/suppliers')
-            .then((response) => {
-                console.log('Fetched suppliers:', response.data);
-                setSuppliers(response.data);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error('Error fetching suppliers:', error);
-                setLoading(false);
-            });
-    }, []);
+        axios
+          .get('http://localhost:5555/employees')
+          .then((response) => {
+            setEmployee(response.data.data);
+            setFilteredEmployee(response.data.data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.log(error);
+            setLoading(false);
+          });
+      }, []);
+
+      useEffect(() => {
+        setFilteredEmployee(
+          employees.filter((employee) =>
+            employee.employeeName.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        );
+      }, [searchTerm]);
 
     return (
         <div style={{ minHeight: '100vh', position: 'relative' }}>
@@ -64,7 +76,7 @@ const PaymentsEmployee = () => {
             </div>
             <div className='p-20' style={{ paddingBottom: '100px' }}>
                 <div className='flex justify-between items-center mb-8'>
-                    <h1 className='text-3xl font-bold text-gray-800'>Supplier Table</h1>
+                    <h1 className='text-3xl font-bold text-gray-800'>Employee Table</h1>
                     <Link
                         to='/suppliers/create'
                         className='bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600 transition-all flex items-center'>
@@ -75,42 +87,49 @@ const PaymentsEmployee = () => {
                 {loading ? (
                     <Spinner />
                 ) : (
-                    <table className='w-full border-collapse border border-gray-300 '>
-                        <thead className='bg-gray-200'>
-                            <tr>
-                                <th className='border border-gray-300 p-4 text-left'>Supplier ID</th>
-                                <th className='border border-gray-300 p-4 text-left'>Name</th>
-                                <th className='border border-gray-300 p-4 text-left'>Address</th>
-                                <th className='border border-gray-300 p-4'>ContactNo</th>
-                                <th className='border border-gray-300 p-4'>Email</th>
-                                <th className='border border-gray-300 p-4'>Operations</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {suppliers.map((item, index) => (
-                                <tr key={item._id} className='border border-gray-300'>
-                                    <td className='border border-gray-300 p-4'>{item.supplierid}</td>
-                                    <td className='border border-gray-300 p-4'>{item.name}</td>
-                                    <td className='border border-gray-300 p-4'>{item.address}</td>
-                                    <td className='border border-gray-300 p-4'>{item.contact}</td>
-                                    <td className='border border-gray-300 p-4'>{item.email}</td>
-                                    <td className='border border-gray-300 p-4'>
-                                        <div className='flex justify-center gap-x-4'>
-                                            <Link to={`/suppliers/details/${item._id}`} className='text-2xl text-green-800'>
-                                                <BsInfoCircle />
-                                            </Link>
-                                            <Link to={`/suppliers/edit/${item._id}`} className='text-2xl text-yellow-600'>
-                                                <AiOutlineEdit />
-                                            </Link>
-                                            <Link to={`/suppliers/delete/${item._id}`} className='text-2xl text-red-600'>
-                                                <MdOutlineDelete />
-                                            </Link>
-                                        </div>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    <div className='overflow-x-auto'>
+            <table className='w-full'>
+              <thead>
+                <tr className='bg-gray-200'>
+                  <th className='px-4 py-2'>No</th>
+                  <th className='px-4 py-2'>Employee Name</th>
+                  <th className='px-4 py-2'>Email Address</th>
+                  <th className='px-4 py-2'>Contact Number</th>
+                  <th className='px-4 py-2'>Address</th>
+                  <th className='px-4 py-2'>Role</th>
+                  <th className='px-4 py-2'>created On</th>
+                  <th className='px-4 py-2'>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredEmployee.map((employee, index) => (
+                  <tr key={employee._id} className={(index % 2 === 0) ? 'bg-gray-100' : 'bg-white'}>
+                    <td className='border px-4 py-2'>{index + 1}</td>
+                    <td className='border px-4 py-2'>{employee.employeeName}</td>
+                    <td className='border px-4 py-2'>{employee.employeeEmail}</td>
+                    <td className='border px-4 py-2'>{employee.employeeMobile}</td>
+                    <td className='border px-4 py-2'>{employee.employeeAddress}</td>
+                    <td className='border px-4 py-2'>{employee.employeeRoles}</td>
+                    <td className='border px-4 py-2'>{new Date(employee.createdOn).toLocaleString()}</td>
+
+                    <td className='border px-4 py-2'>
+                      <div className='flex justify-center gap-x-4'>
+                        <Link to={`/employees/details/${employee._id}`}>
+                          <BsInfoCircle className='text-green-800' />
+                        </Link>
+                        <Link to={`/employees/edit/${employee._id}`}>
+                          <AiOutlineEdit className='text-yellow-600' />
+                        </Link>
+                        <Link to={`/employees/delete/${employee._id}`}>
+                          <MdOutlineDelete className='text-red-600' />
+                        </Link>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
                 )}
             </div>
              {/* Footer */}
